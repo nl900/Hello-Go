1c6f6136ae5a939424ffd804855ef01ebe5acba4
nl900 <nancy.lee900@gmail.com>
2021-09-08 16:42:15 +0200

    add endpoint

--- hello.go
+++ hello.go
@@ -1,12 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
 	r.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, World!")
+	})
+
+	api := r.Group("/api")
+
+	api.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
 	})
 
 	r.Run(":3000")
